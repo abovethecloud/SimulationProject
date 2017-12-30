8095605a10161b9689bb20593ddb9e32d617e3f4
abovethecloud <claudio.peroni@edu.unito.com>
2017-12-30 11:38:44 +0100

    Incremented precision of equality between long doubles.

--- src/utils.c
+++ src/utils.c
@@ -3,7 +3,7 @@
 
 int approx_equal(long double d1, long double d2)
 {
-    const long double epsilon = 0.0000001;  // To compare long double values
+    const long double epsilon = 0.000000000001;  // To compare long double values
     if (fabsl(d1-d2) < epsilon)
         return 1;
     else
